6143118ff4408ded3c3892e3300983577a3cb249
alejandro <alejandro.penil@gmail.com>
2024-05-16 12:38:03 +0200

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.fxtry {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.fxtry to javafx.fxml;
+    exports com.example.fxtry;
+}
