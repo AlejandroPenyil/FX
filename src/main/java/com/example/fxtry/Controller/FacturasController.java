2885de491d578c0299e4a63429f5cd83f25c4197
alejandro <alejandro.penil@gmail.com>
2024-05-20 08:18:07 +0200

    main classes generated and icon added

--- src/main/java/com/example/fxtry/Controller/FacturasController.java
+++ src/main/java/com/example/fxtry/Controller/FacturasController.java
@@ -1,4 +1,123 @@
 package com.example.fxtry.Controller;
 
+import com.example.fxtry.Controller.Create.ClientCreateController;
+import com.example.fxtry.Controller.Update.ClientUpdateController;
+import com.example.fxtry.Model.User;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FacturasController {
+    public static User updatable = new User();
+    //TODO crear variable para realizar el filtrado
+
+    @FXML
+    private TableView<User> tvwClient;
+
+    @FXML
+    private TableColumn<User, String> tcName, tcPassword;
+
+    @FXML
+    private void initialize(){
+        tcName.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNombre()));
+        tcPassword.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getContraseña()));
+//        tvwClient.getItems().add(admin);
+
+        for (int i = 0; i < 50; i++) {
+            User user = new User();
+            user.setNombre("paco"+i);
+            user.setContraseña("10"+i);
+            tvwClient.getItems().add(user);
+        }
+//        tvwClient.refresh();
+    }
+
+    @FXML
+    private void goToMain(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fxtry/main-view.fxml"));
+            Parent secondSceneParent = loader.load();
+            Scene secondScene = new Scene(secondSceneParent);
+            // Acceso al controlador de la segunda escena, si es necesario
+            MainController secondController = loader.getController();
+
+            // Acceso al stage actual
+            Stage currentStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            secondScene.getStylesheets().add(getClass().getResource("/com/example/fxtry/style.css").toExternalForm());
+            // Mostrar la segunda escena en el stage actual
+            currentStage.setScene(secondScene);
+
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goToCreate(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fxtry/Client/client-create-view.fxml"));
+            Parent secondSceneParent = loader.load();
+            Scene secondScene = new Scene(secondSceneParent);
+            // Acceso al controlador de la segunda escena, si es necesario
+            ClientCreateController secondController = loader.getController();
+
+            // Acceso al stage actual
+            MenuItem menuItem = (MenuItem) event.getSource();
+            ContextMenu contextMenu = menuItem.getParentPopup();
+            Scene scene = contextMenu.getOwnerNode().getScene();
+            Stage currentStage = (Stage) scene.getWindow();
+            secondScene.getStylesheets().add(getClass().getResource("/com/example/fxtry/style.css").toExternalForm());
+            // Mostrar la segunda escena en el stage actual
+            currentStage.setScene(secondScene);
+
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goToUpdate(ActionEvent event) {
+        try {
+            User selectedUser = tvwClient.getSelectionModel().getSelectedItem();
+
+            updatable= selectedUser;
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fxtry/Client/client-update-view.fxml"));
+            Parent secondSceneParent = loader.load();
+            Scene secondScene = new Scene(secondSceneParent);
+            // Acceso al controlador de la segunda escena, si es necesario
+            ClientUpdateController secondController = loader.getController();
+
+            // Acceso al stage actual
+            MenuItem menuItem = (MenuItem) event.getSource();
+            ContextMenu contextMenu = menuItem.getParentPopup();
+            Scene scene = contextMenu.getOwnerNode().getScene();
+            Stage currentStage = (Stage) scene.getWindow();
+
+            secondScene.getStylesheets().add(getClass().getResource("/com/example/fxtry/style.css").toExternalForm());
+            // Mostrar la segunda escena en el stage actual
+            currentStage.setScene(secondScene);
+
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(ActionEvent event){
+        User selectedUser = tvwClient.getSelectionModel().getSelectedItem();
+
+        //TODO metodo para softdelete, no va haber hard delete
+    }
 }
